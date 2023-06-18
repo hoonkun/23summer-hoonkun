a9ff530066ee0ef2f3d45c1f4063a11cd8eb2c42
hoonkun <herokun.user@gmail.com>
2023-06-18 21:59:52 +0900

    fix: Post retrieve Metadata 의 이미지 그레디언트를 조금 더 자연스럽게 수정

--- src/app/posts/retrieve/[key]/_generateImageMetadata.tsx
+++ src/app/posts/retrieve/[key]/_generateImageMetadata.tsx
@@ -26,7 +26,7 @@ export default async function MetadataImage({ params }: { params: PostParams }):
           alt={`preview image of post '${post.data.title}' in hoonkun.kiwi`}
           style={{ width: "100%", height: "100%", objectFit: "cover" }}
         />
-        <div style={{ width: "100%", height: "100%", position: "absolute", left: "0", right: "0", background: "radial-gradient(circle at 0% 100%, #000000 0%, #00000000 40%, #00000000 100%)" }}/>
+        <div style={{ width: "100%", height: "100%", position: "absolute", left: "0", right: "0", background: "radial-gradient(circle at 0% 100%, #000000bb 0%, #00000000 40%, #00000000 100%)" }}/>
         <img
           src={`${config.deploy}${(await import("@/resources/logo.png")).default.src}`}
           alt={"logo"}
