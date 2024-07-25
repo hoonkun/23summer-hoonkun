6a67043d5394cb80a44ef88677049db5168e9165
hoonkun <herokun.user@gmail.com>
2024-07-26 02:33:41 +0900

    Revert "fix: 이건가?"
    This reverts commit fecb5f85739d90906836b5ae7351de7ea9c2d87e.

--- src/app/posts/retrieve/[key]/twitter-image.tsx
+++ src/app/posts/retrieve/[key]/twitter-image.tsx
@@ -1,39 +1,5 @@
-import { ImageResponse } from "next/og"
-import { PostParams } from "@/app/posts/retrieve/[key]/page"
-import { Post, Posts } from "@/lib/23summer/post"
-import config from "@/config"
-import { ImageMetadataHeaders } from "@/lib/23summer/ImageMetadataUtils"
+import Image from "./_generateImageMetadata"
 
-export const alt = ""
-export const size = { width: 1200, height: 630 }
+export { alt, size, contentType } from "./_generateImageMetadata"
 
-export const contentType = "image/png"
-
-/* eslint-disable @next/next/no-img-element */
-export default async function MetadataImage({ params }: { params: PostParams }): Promise<ImageResponse> {
-  const post = Posts.retrieve<Post>(params.key, false)
-
-  if (!post) {
-    return new ImageResponse(
-      <div></div>,
-      { ...size }
-    )
-  } else {
-    return new ImageResponse(
-      <div style={{ width: "100%", height: "100%", display: "flex" }}>
-        <img
-          src={`${config.deploy}${(await import(`$/__posts__/${params.key}/preview.jpg`)).default.src}`}
-          alt={`preview image of post '${post.data.title}' in hoonkun.kiwi`}
-          style={{ width: "100%", height: "100%", objectFit: "cover" }}
-        />
-        <div style={{ width: "100%", height: "100%", position: "absolute", left: "0", right: "0", background: "radial-gradient(circle at 0% 100%, #000000bb 0%, #00000000 40%, #00000000 100%)" }}/>
-        <img
-          src={`${config.deploy}${(await import("@/resources/logo.png")).default.src}`}
-          alt={"logo"}
-          style={{ width: "125px", height: "125px", margin: "25px", position: "absolute", left: "0", bottom: "0" }}
-        />
-      </div>,
-      { ...size, headers: ImageMetadataHeaders() }
-    )
-  }
-}
+export default Image;
